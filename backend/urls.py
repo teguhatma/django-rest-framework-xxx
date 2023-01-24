0c11ae2794185f67553ed908d3ee9668ac754ce1
teguhatma <teguh.yudha@finaccel.co>
2023-01-24 22:47:01 +0700

    removed register users in ulrs

--- backend/urls.py
+++ backend/urls.py
@@ -3,13 +3,10 @@ from rest_framework import routers
 from users import urls
 
 router = routers.DefaultRouter()
-# router.register(r'users', views.ListApi, basename='users')
-# router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('users/', include(urls)),
 ]
