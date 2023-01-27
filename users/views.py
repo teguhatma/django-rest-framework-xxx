27c490b9e55cd4c4a8ecea5c297e766c73b8154d
Nurudin Aziz <nrdajis@gmail.com>
2023-01-27 20:38:33 +0700

    'badrequest'

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 
 class Jobs(APIView):
     """
@@ -8,22 +8,30 @@ class Jobs(APIView):
     """
 
     def get(self, request, format=None):
-        data = [
-            {
-                "job": "Backend Engineer",
-                "errors": {},
-                "company_name": "Google",
-                "company_url": "https://www.google.com",
-                "location": "Mountain View, CA",
-                "title": "Backend Engineer"
-            },
-            {
-                "job": "Backend Developer",
-                "errors": {},
-                "company_name": "Google",
-                "company_url": "https://www.google.com",
-                "location": "Mountain View, CA",
-                "title": "Backend Engineer"
-            }
-        ]
-        return Response({"data": data})
+        print(request.query_params.get("id"))
+        if request.query_params.get("error") == "400":
+            return Response({"error": "Bad Request"}, status=status.HTTP_400_BAD_REQUEST)
+        elif request.query_params.get("error") == "500":
+            return Response({"error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            data = [
+                {
+                    "id": 1,
+                    "job": "Backend Engineer",
+                    "errors": {},
+                    "company_name": "Google",
+                    "company_url": "https://www.google.com",
+                    "location": "Mountain View, CA",
+                    "title": "Backend Engineer"
+                },
+                {
+                    "id": 2,
+                    "job": "Backend Developer",
+                    "errors": {},
+                    "company_name": "Google",
+                    "company_url": "https://www.google.com",
+                    "location": "Mountain View, CA",
+                    "title": "Backend Engineer"
+                }
+            ]
+            return Response({"data": data})
